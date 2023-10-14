a64c3b81a49f6f8334e050a52088ad39976b7bdf
thapa-bikram <bthapabikram@gmail.com>
2023-10-14 09:24:21 +0545

    2.1 added

---
+++ 02. Constants, Variables and Data Types/2.1.c
@@ -0,0 +1,24 @@
+/*Write a program to determine and print the sum of the following harmonic series for a given value of n :
+1+1/2+1/3+....+1/n
+*/
+#include<stdio.h>
+int main()
+{
+    int n ;
+    float count = 1;
+    float sum = 0 ;
+    printf("enter the value of n\n");
+    scanf("%d",&n);
+    while(count<=n)
+    {
+        sum=sum+(1/count);
+         printf("The sum of the harmonic series : 1+1/2+1/3+...+1/n is %5.2f\n", sum);
+        count++;
+    }
+    double value = 3.14159;
+    printf("The value is .............\n");
+printf("The value is %10.2f\n", value);
+
+    
+    return 0;
+}
